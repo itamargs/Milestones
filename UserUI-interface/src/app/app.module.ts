2c98c312c6d83e21fd7c4e62bd3400131c5eab08
yakir2a <yakir2a@gmail.com>
2017-06-12 14:51:53 +0300

    remove 2nd nav-bar module
    no need for 2nd nav bar

--- UserUI-interface/src/app/app.module.ts
+++ UserUI-interface/src/app/app.module.ts
@@ -10,7 +10,6 @@ import { AppBodyComponent } from './app-body/app-body.component';
 import { JwtHelper } from './JwtHelper.service';
 import { AccountService } from './accountService.service';
 import { JobHoursComponent } from './app-body/job-hours/job-hours.component';
-import { NavBarComponent } from './app-body/nav-bar/nav-bar.component';
 import { HeaderComponent } from './header/header.component';
 import { WorkTableComponent } from './app-body/job-hours/work-table/work-table.component';
 import { MessageComponent } from './app-body/messages-tab/message/message.component';
@@ -22,7 +21,6 @@ import { MessagesTabComponent } from './app-body/messages-tab/messages-tab.compo
     LoginComponent,
     AppBodyComponent,
     JobHoursComponent,
-    NavBarComponent,
     HeaderComponent,
     WorkTableComponent,
     MessageComponent,
