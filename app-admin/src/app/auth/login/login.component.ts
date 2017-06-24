573dd05dc3b529310d773a4889096086d26ae1af
aviel-sh <ashmaryahu@gmail.com>
2017-06-24 21:06:33 +0300

    update login, reset and register pages
    update forms, and connection to firebase

--- app-admin/src/app/auth/login/login.component.ts
+++ app-admin/src/app/auth/login/login.component.ts
@@ -14,6 +14,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
     console.log(new Date().getDate() + '/' + (new Date().getMonth()+1) + '/' + new Date().getFullYear() + ' | ' + new Date().getHours() + ':' + new Date().getMinutes());
+   
   }
 
   onLogin(form: NgForm) {
