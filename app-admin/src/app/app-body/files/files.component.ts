3d7c2c6038176380af19367343d51fa27cbad3ca
aviel-sh <ashmaryahu@gmail.com>
2017-06-24 23:36:00 +0300

    update information

--- app-admin/src/app/app-body/files/files.component.ts
+++ app-admin/src/app/app-body/files/files.component.ts
@@ -11,20 +11,12 @@ export class FilesComponent implements OnInit {
   constructor(private appBody: AppBodyComponent) { }
 
   ngOnInit() {
-     console.log(this.allow());
+     
   }
 
   allow() {
     return String(this.appBody.user.type)==='0';
   }
-  fileEvent(fileInput: any){
-    let file = fileInput.target.files[0];
-    let fileName = file.name;
-    console.log(fileInput);
-    firebase.storage().ref().put(fileInput)
-    .then(
-      (snapshot)=>console.log(snapshot)
-    )
-}
+  
 
 }
