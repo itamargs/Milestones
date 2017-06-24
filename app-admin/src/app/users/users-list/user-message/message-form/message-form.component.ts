1b4d4e35e5d9366909b24c91308b6e9ab83a8c8b
aviel-sh <ashmaryahu@gmail.com>
2017-06-24 21:07:41 +0300

    messages form
    create forms for admin to send mwssages to users

---
+++ app-admin/src/app/users/users-list/user-message/message-form/message-form.component.ts
@@ -0,0 +1,38 @@
+import { UserService } from './../../../user.service';
+import { NgForm } from '@angular/forms';
+import { User } from './../../../user.model';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-message-form',
+  templateUrl: './message-form.component.html',
+  styleUrls: ['./message-form.component.css']
+})
+export class MessageFormComponent implements OnInit {
+  @Input() users: User[]
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    
+  }
+
+  onSend(form: NgForm) {
+    let newDate = '';
+    newDate = String(new Date().getDate() + '/' + (new Date().getMonth()+1) + '/'
+                + new Date().getFullYear() + ' | ');
+    if(new Date().getHours() < 10)
+      newDate += '0';
+    newDate += String(new Date().getHours()) + ':';
+    if(new Date().getMinutes() < 10)
+      newDate += '0';
+    newDate += String(new Date().getMinutes());
+    console.log(newDate);
+    console.log(form.value.title);
+    console.log(form.value.body);
+    this.userService.sendUserMessage(this.users, form.value.title, form.value.body, newDate);
+    form.reset();
+    window.alert('הודעה נשלחה בהצלחה!')
+  }
+
+
+}
--- app-admin/src/app/users/users-list/user-message/user-message.component.ts
+++ app-admin/src/app/users/users-list/user-message/user-message.component.ts
@@ -1,3 +1,6 @@
+import { NgForm } from '@angular/forms';
+import { User } from './../../user.model';
+import { UserService } from './../../user.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-message.component.css']
 })
 export class UserMessageComponent implements OnInit {
-
-  constructor() { }
+  users: User[];
+  userSelected = [];
+  userText = '';
+  error: string;
+  type: string;
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.users = this.userService.getUsers();
+  }
+  selectUser(user: User) {
+    this.error = null;
+    for(var i=0; i<this.userSelected.length; i++){
+      if(user===this.userSelected[i])
+        this.error = "משתמש זה כבר נבחר";
+    }
+    if(this.error == null) {
+      this.userText += user.firstName + ' ' + user.lastName + ', ';
+      this.userSelected.push(user);
+      console.log(this.userSelected[0].firstName);
+    }
+  }
+
+  onSubmit(form: NgForm) {
+    this.userSelected = [];
+    this.type = String(form.value.type);
+    for(var i=0; i<this.users.length; i++)
+        if(String(this.type)===String(this.users[i].type))
+          this.userSelected.push(this.users[i]);
+    if(this.type==='0') {
+      this.userText = 'חברי עמק תפארת';
+    }
+    else if(this.type==='1'){
+      this.userText = 'חברי קטיף ישראלי';
+    }
+    else{
+      this.userSelected = [];
+      this.userText = '';
+    }
+    console.log(this.type);
+  }
+
+  onReset() {
+    this.userSelected = [];
+    this.userText = '';
+    console.log(this.userSelected);
   }
 
 }
